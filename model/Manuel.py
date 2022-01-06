f11f28c31dd598cad578c64553b1709c987a771c
Angelo Samina <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2022-01-06 18:40:02 +0100

    Ecriture de la fonction choisirCaseTirManuel

--- model/Manuel.py
+++ model/Manuel.py
@@ -15,3 +15,15 @@ def placerBateauxManuel (joueur : dict) -> None :
     window.placer_bateaux()
 
     return None
+########################################################################################################################
+def choisirCaseTirManuel(joueur : dict) -> tuple :
+    if not type_joueur(joueur):
+        raise ValueError(f"Le paramètre {joueur} ne correspond pas à un joueur.")
+
+    nomJoueur = getNomJoueur(joueur)
+
+    window.afficher(joueur)
+    window.display_message(f"{nomJoueur} : Choisissez la case où vous voulez tirer." )
+    window.set_action(f"Choisissez la case de tir (bouton gauche")
+    a = window.get_clicked_cell(2)
+    return a
